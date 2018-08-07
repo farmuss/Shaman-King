8da018ccd110b4782ff982ccf875cecfcff5da6e
farmuss <farhod2307@hotmail.com>
2018-08-07 00:38:54 -0600

    Added 2 Characters

--- java.js
+++ java.js
@@ -1,3 +1,35 @@
-// $(document).ready(function() {
-//     $("#header").html("WORKS!");
-// });
+var yoh = {
+    name: "Yoh Asakura",
+    picture: '<img src="images/yoh.jpg">',
+    hp: 100 
+}
+
+var rio = {
+    name: "Rio",
+    picture: "Blank for now",
+    hp: 100 
+}
+
+var len = {
+    name: "Len Tao",
+    picture: "Blank for now",
+    hp: 100 
+}
+
+var zeke = {
+    name: "Zeke",
+    picture: '<img src="images/zeke.jpg">',
+    hp: 300 
+}
+
+$(document).ready(function() {
+    $("#test").on("click", function() {
+        $("#attackerName").html(yoh.name);
+        $("#attackerImg").html(yoh.picture);
+        $("#attackerHp").html(yoh.hp);
+        $("#defenderName").html(zeke.name);
+        $("#defenderImg").html(zeke.picture);
+        $("#defenderHp").html(zeke.hp);
+    });
+});
+

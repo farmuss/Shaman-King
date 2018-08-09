9ed4eb9b2b3596b016d43ed40295d168f11f72ad
farmuss <farhod2307@hotmail.com>
2018-08-09 01:49:10 -0600

    Major Changes

--- java.js
+++ java.js
@@ -1,47 +1,88 @@
-alert("WORK IN PROGRESS! :D");
+$(document).ready(function () {
+    var wolf = Math.floor(Math.random() * 11) + 1;
+    var panda = Math.floor(Math.random() * 11) + 1;
+    var eagle = Math.floor(Math.random() * 11) + 1;
+    var tiger = Math.floor(Math.random() * 11) + 1;
+    var currentNumber = 0;
+    var randomNumber = Math.floor(Math.random() * 101) + 29;
+    console.log(randomNumber);
+    var wins = 0;
+    var losses = 0;
 
-var yoh = {
-    name: "Yoh Asakura",
-    picture: '<img src="images/yoh.jpg">',
-    hp: 100 
-}
+    function resetRound() {
+        $("#currentNumber").text(0);
+        currentNumber = 0;
+        randomNumber = Math.floor(Math.random() * 101) + 29;
+        
+    }
 
-var rio = {
-    name: "Rio",
-    picture: "Blank for now",
-    hp: 100 
-}
+        $("#randomNumber").append(randomNumber);
+    
+    
+        $("#wolf").on("click", function () {
+            currentNumber = currentNumber+wolf;
+            $("#currentNumber").text(currentNumber);
+            $("#wolfStart").text(wolf); 
+            if (currentNumber === randomNumber) {
+                resetRound();
+                $("#wins").html(wins+1);
+                alert("YOU WON");
 
-var jun = {
-    name: "Len Tao",
-    picture: "Blank for now",
-    hp: 100 
-}
+ 
+             } else if (currentNumber > randomNumber){
+                 $("#losses").html(losses+1);
+                 resetRound();
+             }
+             
+             console.log(currentNumber);        
+        });
+    
+        $("#panda").on("click", function () {
+            currentNumber = currentNumber+panda;
+            $("#currentNumber").text(currentNumber);
+            $("#pandaStart").text(panda);
+            if (currentNumber === randomNumber) {
+               $("#wins").html(wins+1);
 
-var zeke = {
-    name: "Zeke",
-    picture: '<img src="images/zeke.jpg">',
-    hp: 300 
-}
+            } else if (currentNumber > randomNumber){
+                $("#losses").html(losses+1);
+            }
 
-$(document).ready(function() {
-    $("#test").on("click", function() {
-        $("#attackerName").html(yoh.name);
-        $("#attackerImg").html(yoh.picture);
-        $("#attackerHp").html(yoh.hp);
-        $("#defenderName").html(zeke.name);
-        $("#defenderImg").html(zeke.picture);
-        $("#defenderHp").html(zeke.hp);
-    });
+            console.log(currentNumber);        
+        });
+    
+        $("#eagle").on("click", function () {
+            currentNumber = currentNumber+eagle;
+            $("#currentNumber").text(currentNumber);
+            $("#eagleStart").text(eagle);
+            if (currentNumber === randomNumber) {
+                $("#wins").html(wins+1);
+ 
+             } else if (currentNumber > randomNumber){
+                 $("#losses").html(losses+1);
+             }
+ 
+            console.log(currentNumber);        
+        });
+    
+        $("#tiger").on("click", function () {
+            currentNumber = currentNumber+tiger;
+            $("#currentNumber").text(currentNumber);
+            $("#tigerStart").text(tiger);
+            if (currentNumber === randomNumber) {
+                resetRound();
+                $("#wins").html(wins+1);
+                
+ 
+             } else if (currentNumber > randomNumber){
+                 $("#losses").html(losses+1);
+             }
+ 
+            console.log(currentNumber);        
+        });   
+                
 });
-$(document).ready(function() {
-$("#reset").on("click", function(){
-    $("#attackerName").html("Name");
-    $("#attackerImg").html(" ");
-    $("#attackerHp").html("HP");
-    $("#defenderName").html("Name");
-    $("#defenderImg").html(" ");
-    $("#defenderHp").html("HP");
 
-});
-});
+
+
+

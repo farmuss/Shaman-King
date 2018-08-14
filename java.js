35587fb62b806fba3d79a2c869e4a9aa89274e7e
farmuss <farhod2307@hotmail.com>
2018-08-13 20:17:59 -0600

    Final Commit

--- java.js
+++ java.js
@@ -1,22 +1,36 @@
-alert("Work in Progress :D");
+// alert("Work in Progress :D");
 
 $(document).ready(function () {
-    var wolf = Math.floor(Math.random() * 11) + 1;
-    var panda = Math.floor(Math.random() * 11) + 1;
-    var eagle = Math.floor(Math.random() * 11) + 1;
-    var tiger = Math.floor(Math.random() * 11) + 1;
+    var wolf = Math.floor(Math.random() * 12) + 2;
+    var panda = Math.floor(Math.random() * 12) + 2;
+    var eagle = Math.floor(Math.random() * 12) + 2;
+    var tiger = Math.floor(Math.random() * 12) + 2;
     var currentNumber = 0;
     var randomNumber = Math.floor(Math.random() * 101) + 29;
     console.log(randomNumber);
     var wins = 0;
     var losses = 0;
+    var userNumber;
+
+
+
 
     function resetRound() {
+        alert("Resetting the Round");
         $("#currentNumber").text(0);
+        $("#wolfStart").text("");
+        $("#pandaStart").text("");
+        $("#eagleStart").text("");
+        $("#tigerStart").text("");
         currentNumber = 0;
+        wolf = Math.floor(Math.random() * 12) + 2;
+        panda = Math.floor(Math.random() * 12) + 2;
+        eagle = Math.floor(Math.random() * 12) + 2;
+        tiger = Math.floor(Math.random() * 12) + 2;  
         randomNumber = Math.floor(Math.random() * 101) + 29;
-        
+        $("#randomNumber").text(randomNumber);        
     }
+
 
         $("#randomNumber").append(randomNumber);
     
@@ -25,14 +39,18 @@ $(document).ready(function () {
             currentNumber = currentNumber+wolf;
             $("#currentNumber").text(currentNumber);
             $("#wolfStart").text(wolf); 
+            
             if (currentNumber === randomNumber) {
-                resetRound();
-                $("#wins").html(wins+1);
+                wins++;
+                $("#wins").html(wins);
                 alert("YOU WON");
+                resetRound();
 
  
              } else if (currentNumber > randomNumber){
-                 $("#losses").html(losses+1);
+                 losses++;
+                 $("#losses").html(losses);
+                 alert("YOU LOST");
                  resetRound();
              }
              
@@ -44,11 +62,18 @@ $(document).ready(function () {
             $("#currentNumber").text(currentNumber);
             $("#pandaStart").text(panda);
             if (currentNumber === randomNumber) {
-               $("#wins").html(wins+1);
+                wins++;
+                $("#wins").html(wins);
+                alert("YOU WON");
+                resetRound();
 
-            } else if (currentNumber > randomNumber){
-                $("#losses").html(losses+1);
-            }
+ 
+             } else if (currentNumber > randomNumber){
+                 losses++;
+                 $("#losses").html(losses);
+                 alert("YOU LOST");
+                 resetRound();
+             }
 
             console.log(currentNumber);        
         });
@@ -58,10 +83,17 @@ $(document).ready(function () {
             $("#currentNumber").text(currentNumber);
             $("#eagleStart").text(eagle);
             if (currentNumber === randomNumber) {
-                $("#wins").html(wins+1);
+                wins++;
+                $("#wins").html(wins);
+                alert("YOU WON");
+                resetRound();
+
  
              } else if (currentNumber > randomNumber){
-                 $("#losses").html(losses+1);
+                 losses++;
+                 $("#losses").html(losses);
+                 alert("YOU LOST");
+                 resetRound();
              }
  
             console.log(currentNumber);        
@@ -72,12 +104,17 @@ $(document).ready(function () {
             $("#currentNumber").text(currentNumber);
             $("#tigerStart").text(tiger);
             if (currentNumber === randomNumber) {
+                wins++;
+                $("#wins").html(wins);
+                alert("YOU WON");
                 resetRound();
-                $("#wins").html(wins+1);
-                
+
  
              } else if (currentNumber > randomNumber){
-                 $("#losses").html(losses+1);
+                 losses++;
+                 $("#losses").html(losses);
+                 alert("YOU LOST");
+                 resetRound();
              }
  
             console.log(currentNumber);        
